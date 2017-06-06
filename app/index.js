52cb4fa040478f8ee46ac2a528832045ff6e4e56
张大漾丨前端开发 <1417376255@qq.com>
2017-06-06 19:49:57 +0800

    auto update

--- app/index.js
+++ app/index.js
@@ -1,5 +1,6 @@
 import 'github-markdown-css'
 import './Components/index.css'
+import './Components/print.css'
 import './Components/Header.css'
 import './Components/Footer.css'
 import { yhtml5 } from '../build/template/author'
